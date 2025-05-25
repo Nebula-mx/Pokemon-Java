bb92d253216e1cebe9eb8a5faafcac4810674155
Emiliano Candia Carrillo <cemiliano382@gmail.com>
2025-05-24 19:57:07 -0600

    nuevoMain.java

--- Main.java
+++ Main.java
@@ -11,24 +11,62 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+package pokemon;
+
+import pokemon.controller.Batalla;
+import pokemon.model.pokemon.instances.Charmander;
+import pokemon.model.pokemon.instances.Bulbasaur;
+import pokemon.model.pokemon.instances.Squirtle;
+import pokemon.utils.Guardado;
+import javafx.application.Application;
+import java.util.*;
+
 public class Main {
-
     public static void main(String[] args) {
-        app.main();
+        // Inicia JavaFX (tu código original)
+        Application.launch(app.class, args);
+
+        // Código nuevo: Menú de consola adicional
+        iniciarMenuConsola();
+    }
+
+    private static void iniciarMenuConsola() {
         Charmander charmander = new Charmander();
-        Bulbasaur bulbasour = new Bulbasaur();
+        Bulbasaur bulbasaur = new Bulbasaur();
         Squirtle squirtle = new Squirtle();
+        Scanner scanner = new Scanner(System.in);
 
-        Map<String, Function<Integer, Integer>> damageTargets = new HashMap<>();
-        damageTargets.put("charmander", charmander::getHurt);
-        damageTargets.put("bulbasour", bulbasour::getHurt);
-        damageTargets.put("squirtle", squirtle::getHurt);
+        while (true) {
+            System.out.println("\n--- MENÚ CONSOLA ---");
+            System.out.println("1. Ver Pokémon");
+            System.out.println("2. Batalla automática");
+            System.out.println("3. Guardar partida");
+            System.out.println("4. Salir");
+            System.out.print("Elige: ");
 
-        charmander.showInfo();
-        squirtle.showInfo();
-        damageTargets.get("charmander").apply(squirtle.doMovement(1));
-        charmander.showInfo();
-        damageTargets.get("squirtle").apply(charmander.doMovement(3));
-        squirtle.showInfo();
+            int opcion = scanner.nextInt();
+            switch (opcion) {
+                case 1:
+                    charmander.showInfo();
+                    bulbasaur.showInfo();
+                    squirtle.showInfo();
+                    break;
+
+                case 2:
+                    Batalla.iniciar(charmander, squirtle);  // Ejemplo: Charmander vs Squirtle
+                    break;
+
+                case 3:
+                    AbstractPokemon[] equipo = {charmander, bulbasaur, squirtle};
+                    Guardado.guardarEquipo(equipo, "partida.dat");
+                    break;
+
+                case 4:
+                    System.exit(0);
+
+                default:
+                    System.out.println("Opción inválida.");
+            }
+        }
     }
 }
